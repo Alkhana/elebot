83bd09438f2b3e60541c08ef6f1b29d7d3382ac2
Alkhana <ally.doll.neer@gmail.com>
2018-07-09 00:36:49 +0300

    Add files via upload

---
+++ libraries/PSX/examples/PSX.ino
@@ -0,0 +1,33 @@
+// PSX.ino
+// Sample for PSX lib
+// (c) 2017 Al'Khana
+
+#include "PSX.h"
+
+//#define LEDPin 13
+
+PSXClass PSX(8, 9, 11, 10);
+
+uint8_t data = 0;
+
+void setup()
+{
+  psx.begin();
+  //pinMode(LEDPin, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  uint8_t cdat = PSX.read();
+  if(cdat != data)
+  {
+    data = cdat;
+    Serial.print(psx.mode);
+    Serial.print(": ");
+    Serial.println(PSX.buttons);
+    Serial.print("PSA_LY :");
+    Serial.println(PSX.pressed[PSA_LY]);
+  }
+  delay(20);
+}
